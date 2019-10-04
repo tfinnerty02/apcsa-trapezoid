7fc3d72db732596eccf0049108afb2351fa2bed5
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2019-10-04 13:31:25 -0400

    Update Area.java
    I committed my rectangle code twice. This code is from the computer I took the test on. If you need to see the date last modified on the folder and that it was the day of the test I can sign in on that computer.

--- Area.java
+++ Area.java
@@ -4,15 +4,17 @@ public class Area {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
    
-    System.out.print("\nEnter a length: ");
-    double length = in.nextDouble();
+    System.out.print("\nEnter a top length: ");
+    double topLength = in.nextDouble();
     
-    System.out.print("Enter a width: ");
-    double width = in.nextDouble();
+    System.out.print("Enter a bottom length: ");
+    double bottomLength = in.nextDouble();
     
-    double area = length * width;
-    System.out.print("\nThe area of a " + length + "-by-" + width + " rectangle is ");
-    System.out.printf("%.1f", area);
+    System.out.print("Enter a height: ");
+    double height = in.nextDouble();
+    
+    double area = (( topLength + bottomLength)/2) * height;
+    System.out.printf("\nThe area of the trapezoid is %.1f", area);
     System.out.print(" square units.\n");
     
     in.close();
